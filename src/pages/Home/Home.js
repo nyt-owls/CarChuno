4954f6411da2d673f0a9207734b11af34b85e8a7
Mayank Shekhar <mayankshekhar03@gmail.com>
2021-12-25 20:04:17 +0530

    import improvements

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,17 @@
-import "./Home.css";
+import styles from "./Home.module.css";
 import { ReactComponent as Chuno } from "../../assets/images/car-chuno.svg";
 
 function Home() {
-	return (
-		<div className="home_page">
-			<h1 className="app_name">Car Chuno</h1>
-			<Chuno className="hero_image" />
-			<p className="page_description">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-			</p>
-			<button className="chuno_btn">CHUNO</button>
-		</div>
-	);
+  return (
+    <div className={styles.home_page}>
+      <h1 className={styles.app_name}>Car Chuno</h1>
+      <Chuno className={styles.hero_image} />
+      <p className={styles.page_description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <button className={styles.chuno_btn}>CHUNO</button>
+    </div>
+  );
 }
 
 export default Home;
