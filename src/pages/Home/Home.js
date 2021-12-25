dd617da1fc48c5983e5b8b7da214a368ed62a24a
Mayank Shekhar <mayankshekhar03@gmail.com>
2021-12-25 22:31:31 +0530

    added homepage animations

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,24 @@
 import React from "react";
 import styles from "./Home.module.css";
 import { ReactComponent as Chuno } from "../../assets/images/car-chuno.svg";
+import { Link } from "react-router-dom";
+import { Fade, Flip } from "react-reveal";
 
 function Home() {
   return (
     <div className={styles.home_page}>
       <h1 className={styles.app_name}>Car Chuno</h1>
-      <Chuno className={styles.hero_image} />
+      <Fade>
+        <div className={styles.hero_image}>
+          <Chuno className={styles.image} />
+        </div>
+      </Fade>
       <p className={styles.page_description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <Flip cascade>All the information related to cars in one place.</Flip>
       </p>
-      <button className={styles.chuno_btn}>CHUNO</button>
+      <Link to="/cars">
+        <button className={styles.chuno_btn}>Chuno</button>
+      </Link>
     </div>
   );
 }
--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -2,5 +2,9 @@ import React from "react";
 import styles from "./List.module.css";
 
 export default function List() {
-  return <div className={styles.page}>Hello World</div>;
+  return (
+    <div className={styles.page}>
+      <div>Hello</div>
+    </div>
+  );
 }
