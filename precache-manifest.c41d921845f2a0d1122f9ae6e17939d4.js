95ebfd35b1873812cbecb10c953f997230359175
shekharmayank <shekharmayank@users.noreply.github.com>
2021-12-26 04:19:47 +0000

    deploy: dd617da1fc48c5983e5b8b7da214a368ed62a24a

---
+++ precache-manifest.c41d921845f2a0d1122f9ae6e17939d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f2e2032b18f7dfc6a2f",
+    "url": "/CarChuno/static/css/main.99f76ff0.chunk.css"
+  },
+  {
+    "revision": "9f2e2032b18f7dfc6a2f",
+    "url": "/CarChuno/static/js/main.9f2e2032.chunk.js"
+  },
+  {
+    "revision": "4459c0e030c2a41b41e4",
+    "url": "/CarChuno/static/js/1.4459c0e0.chunk.js"
+  },
+  {
+    "revision": "f75a3f953edfd0f5d58e",
+    "url": "/CarChuno/static/js/2.f75a3f95.chunk.js"
+  },
+  {
+    "revision": "d44c477bac90e6038834",
+    "url": "/CarChuno/static/js/runtime~main.d44c477b.js"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/CarChuno/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/CarChuno/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "8438cdf68098ce694423b472b026985c",
+    "url": "/CarChuno/static/media/car-chuno.8438cdf6.svg"
+  },
+  {
+    "revision": "759e8b8f68fbc309fa78c2cedd42b0db",
+    "url": "/CarChuno/index.html"
+  }
+];
